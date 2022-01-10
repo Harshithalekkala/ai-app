2089014899a2709a1f49b8f8f39c66dac21d756f
Harshithalekkala <80406614+Harshithalekkala@users.noreply.github.com>
2022-01-10 17:47:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+    lipstick_filter=loadImage('https://i.postimg.cc/hvLxFsr8/red-lips.jpg');
 }
 
 function setup() {
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("model is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results);
+        console.log(results[0].pose.nose.x);
+        console.log(results[0].pose.nose.y);
+        noseX=results [0].pose.nose.x;
+        noseY=results [0].pose.nose.y;
+        
+
+    }
 }
 
 function draw() {
+    image(video, 0, 0, 300, 300);
+    image(lipstick_filter, noseX-5, noseY, 30, 30);
 }
 
 function takesnapshot() {
